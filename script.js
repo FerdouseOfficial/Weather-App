70bd32693d03315ff68be7b21f2831aff4bf8ab9
FerdouseOfficial <mollajihad402@gmail.com>
2025-10-08 21:56:47 +0600

    almost done

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-let 
+let button = document.getElementById("submit");
+let input = document.getElementById("userInput");
+
+let cityName = document.getElementById("cityName");
+let cityTime = document.getElementById("cityTime");
+let cityTemp = document.getElementById("cityTemp");
+
+async function getData(cityName) {
+  let promise = await fetch(
+    `http://api.weatherapi.com/v1/current.json?key=8187652ff5d34a8283a123118250810&q=${cityName}&aqi=yes`
+  );
+}
+
+button.addEventListener("click", async () => {
+  let value = input.value;
+  let result = await getData(value);
+});
