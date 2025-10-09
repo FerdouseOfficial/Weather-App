691ae776f0519b89ce656a9775c4847ae640d579
FerdouseOfficial <mollajihad402@gmail.com>
2025-10-09 08:23:42 +0600

    Solving Problem

--- script.js
+++ script.js
@@ -5,10 +5,11 @@ let cityName = document.getElementById("cityName");
 let cityTime = document.getElementById("cityTime");
 let cityTemp = document.getElementById("cityTemp");
 
-async function getData(cityName) {
+async function getData(city) {
   let promise = await fetch(
-    `http://api.weatherapi.com/v1/current.json?key=8187652ff5d34a8283a123118250810&q=${cityName}&aqi=yes`
+    `http://api.weatherapi.com/v1/current.json?key=8187652ff5d34a8283a123118250810&q=${city}&aqi=yes`
   );
+  return await promise.json();
 }
 
 button.addEventListener("click", async () => {
